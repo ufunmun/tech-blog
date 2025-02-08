50c4a5d2bac5c6c2b297050909da47b44946a0ea
ufunmun <saikyou_6@yahoo.co.jp>
2025-02-09 01:55:35 +0900

    chore：　eslint、prettierの設定を追加

---
+++ eslint.config.js
@@ -0,0 +1,43 @@
+import eslintPluginVue from 'eslint-plugin-vue';
+import tsParser from '@typescript-eslint/parser';
+import * as vueParser from 'vue-eslint-parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['node_modules', 'dist', '.nuxt'],
+  },
+  {
+    files: ['**/*.{js,ts,vue}'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        extraFileExtensions: ['.vue'],
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      vue: eslintPluginVue,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+      'vue/html-indent': ['error', 2],
+      'vue/html-self-closing': 'error',
+      'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+      'vue/no-unused-components': 'warn',
+      'vue/max-attributes-per-line': [
+        'error',
+        {
+          singleline: 3,
+          multiline: 1,
+        },
+      ],
+    },
+  },
+  prettier,
+];
